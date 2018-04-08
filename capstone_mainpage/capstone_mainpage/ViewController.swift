699e003afec7c87d11036b719190eeb818b0fe7b
이혜리 <heojunhyeong@heojunhyeong-ui-macbook-pro.local>
2018-04-08 17:11:46 +0900

    Backbone of the application. This enables the change of two pages : registration and recognition of stickers. Create view controllers and pass them to page view controller.

--- capstone_mainpage/capstone_mainpage/ViewController.swift
+++ capstone_mainpage/capstone_mainpage/ViewController.swift
@@ -9,19 +9,25 @@
 import UIKit
 import Parchment
 
+// Create two  view controllers, one is for 'registration of sticker' and the other for 'recognizing sticker',
+// and pass them into our paging view controller.
+// FixedPagingViewController is a subclass of PagingViewController for fixed number of views.
+// It creates a data source that manages setting up paging items and displaying view controller titles.
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Create two view controllers and pass them to pagingViewcontroller.
+        let viewControllers = (0...2).map { mainViewController(index: $0) }
+        let pagingViewController = FixedPagingViewController(viewControllers: viewControllers)
+        
+        // Add created view controllers
+        addChildViewController(pagingViewController)
+        view.addSubview(pagingViewController.view)
+        view.constrainToEdges(pagingViewController.view)
+        pagingViewController.didMove(toParentViewController: self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
