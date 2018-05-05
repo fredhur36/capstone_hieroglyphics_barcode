9bcb3f527daf52461c384a4e9325fb3e90d94017
이혜리 <heojunhyeong@heojunhyeong-ui-macbook-pro.local>
2018-05-05 11:24:51 +0900

    To add the view : declare a view Add object put constraints

--- capstone_mainpage/capstone_mainpage/ViewController.swift
+++ capstone_mainpage/capstone_mainpage/ViewController.swift
@@ -19,29 +19,86 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = UIColor.blue
+        
         //add Horizontal StackView
         //let StackView = UIStackView()
         //StackView.axis = .vertical
+        //StackView.distribution = .fill
+        //StackView.backgroundColor = UIColor.red
+        //view.addSubview(StackView)
+        //StackView.translatesAutoresizingMaskIntoConstraints =
+        //StackView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        //StackView.leadingAnchor.constraint(equalTo:view.leadingAnchor).isActive = true
+        //StackView.heightAnchor.constraint(equalTo:view.heightAnchor).isActive = true
+        //StackView.trailingAnchor.constraint(equalTo:view.trailingAnchor).isActive = true
         
+        // Put camera here
+        let cameraScreen = UIView()
+        cameraScreen.backgroundColor = UIColor.gray
+        let label = UILabel()
+        label.text = "cameraScreen"
+        cameraScreen.addSubview(label)
+        //cameraScreen.constrainCentered(label)
+        //cameraScreen.constrainToEdges(view)
+        //View for cameraShotButton and storageButton
+        cameraScreen.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        let bottomView = UIView()
+        bottomView.backgroundColor = UIColor.red
+        
+        let label2 = UILabel()
+        label2.text = "bottomView"
+        bottomView.addSubview(label2)
+        bottomView.constrainCentered(label2)
+        
+        let cameraShotButton = UIButton()
+        cameraShotButton.backgroundColor = UIColor.white
+        cameraShotButton.clipsToBounds = true
 
+        let storageButton = UIButton()
+        storageButton.setTitle("Storage", for : .normal)
+        storageButton.backgroundColor = UIColor.white
+        storageButton.clipsToBounds = false
+
+        // parchment
         // Create two view controllers and pass them to pagingViewcontroller.
         let viewControllers = (0...1).map { MainViewController(index: $0) }
         let pagingViewController = FixedPagingViewController(viewControllers: viewControllers)
+        //let parchment = pagingViewController.view!
         
         
-        // Camera and the sign where to put the sticker in.
-        // Camera button and Button to the storage
-        //let cameraButton = UIButton()
-        //let storageButton = UIButton()
-
-
+        view.addSubview(cameraScreen)
+        
+        /*
+        view.addSubview(parchment)
+        view.addSubview(bottomView)
+        view.addSubview(cameraShotButton)
+        view.addSubview(storageButton)
+         */
         // Add created view controllers
         addChildViewController(pagingViewController)
-        //view.addSubview(StackView)
-        //StackView.addArrangedSubview(label)
-        view.addSubview(pagingViewController.view)
-        view.constrainToEdges(pagingViewController.view)
+        
+        //StackView.addArrangedSubview(cameraScreen)
+        cameraScreen.topAnchor.constraint(equalTo : view.topAnchor).isActive = true
+        cameraScreen.widthAnchor.constraint(equalTo : view.widthAnchor).isActive = true
+        cameraScreen.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        /*
+        //StackView.addArrangedSubview(parchment)
+        parchment.topAnchor.constraint(equalTo:cameraScreen.bottomAnchor).isActive = true
+        //StackView.addArrangedSubview(bottomView)
+        bottomView.topAnchor.constraint(equalTo:parchment.bottomAnchor).isActive = true
+        
+        */
+
+        
+
+        //view.addSubview(pagingViewController.view)
+        //view.constrainToEdges(pagingViewController.view)
         pagingViewController.didMove(toParentViewController: self)
     }
+    
     
 }
