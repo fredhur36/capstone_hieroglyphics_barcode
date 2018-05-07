abf3681dbc0aef68a45676658dde2289ababae87
Narruu <esara2021@gmail.com>
2018-05-07 12:33:34 +0900

    add pod file

---
+++ capstone_mainpage/Pods/Target Support Files/Pods-capstone_mainpage/Pods-capstone_mainpage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_capstone_mainpage : NSObject
+@end
+@implementation PodsDummy_Pods_capstone_mainpage
+@end
---
+++ capstone_mainpage/Pods/Target Support Files/Pods-capstone_mainpageUITests/Pods-capstone_mainpageUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_capstone_mainpageUITests : NSObject
+@end
+@implementation PodsDummy_Pods_capstone_mainpageUITests
+@end
